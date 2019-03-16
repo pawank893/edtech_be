401ad59179d2abdde9b5571c36cfb19ed6fa7686
Pawan <pawan@headout.com>
2019-03-16 16:09:10 +0530

    production env

--- edtech/settings/prod.py
+++ edtech/settings/prod.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from common import Common
+from edtech.settings import log_conf
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -10,3 +11,4 @@ class Prod(Common):
     DEBUG = False
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+    LOGGING = log_conf.get(log_root='/var/log/edtech/')
